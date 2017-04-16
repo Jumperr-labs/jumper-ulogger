2edbe48111159815623f2c09604ad2a839f62d1f
dansheme <dan.shemesh@gmail.com>
2017-04-16 12:02:21 +0300

    signatures

--- ulogger/ulogging_buffer.c
+++ ulogger/ulogging_buffer.c
@@ -1,5 +1,12 @@
-//
-// Created by Dan Shemesh on 16/04/2017.
-//
-
 #include "ulogging_buffer.h"
+
+typedef uLoggingBuffer uBuffer;
+
+
+int ulogging_buffers_init(ulogging_buffer, start, buffer_capacity) {
+    for
+}
+
+void push_event_to_buffer(EventType event_type, timestamp time, ...) {
+
+}
--- ulogger/ulogging_buffer.h
+++ ulogger/ulogging_buffer.h
@@ -1,8 +1,18 @@
-//
-// Created by Dan Shemesh on 16/04/2017.
-//
+#include "ubuffer.h"
 
 #ifndef JUMPER_ULOGGER_ULOGGING_BUFFER_H
 #define JUMPER_ULOGGER_ULOGGING_BUFFER_H
+
+#ifndef NUM_OF_LOGGING_BUFFERS
+#define NUM_OF_LOGGING_BUFFERS 1
+#endif
+
+uLoggingBuffer ulogging_buffers[NUM_OF_LOGGING_BUFFERS];
+
+ulogging_buffer_init(ulogging_buffer, start, buffer_capacity);
+
+void push_event_to_buffer(EventType event_type, timestamp time, ...);
+
+
 
 #endif //JUMPER_ULOGGER_ULOGGING_BUFFER_H
