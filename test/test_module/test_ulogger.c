de799593de1359b2296923d5e4224a2310f633db
dansheme <dan.shemesh@gmail.com>
2017-04-19 11:19:46 +0300

    tests are verifying that handler was called

--- test/test_module/test_ulogger.c
+++ test/test_module/test_ulogger.c
@@ -6,40 +6,38 @@
 
 TEST_GROUP(TestLogger);
 
-static uint32_t counter;
+static uint32_t timestamp_counter, log_handler_counter;
 
 TEST_SETUP(TestLogger) {
-    counter = (uint32_t) -1;
+    timestamp_counter = (uint32_t) -1;
+    log_handler_counter = (uint32_t) -1;
 }
 
 TEST_TEAR_DOWN(TestLogger) {
 }
 
 void get_timestamp(timestamp *data) {
-    *data = (timestamp) ++counter;
+    *data = (timestamp) ++timestamp_counter;
 }
 
 void log_handler(EventType event_type, timestamp time, ...) {
-//    printf("Event: %d\nTime: %d\n", event_type, time);
+    log_handler_counter = (uint32_t) time;
+}
+
+void test_counters(uint32_t expected_value) {
+    TEST_ASSERT_EQUAL(expected_value, timestamp_counter);
+    TEST_ASSERT_EQUAL(expected_value, log_handler_counter);
 }
 
 TEST(TestLogger, Test_Sanity) {
     uLogger logger;
     void* handler_data[] = {NULL};
-    handler_func log_handlers[1] = {&log_handler};
+    handler_func log_handlers[1] = {(handler_func) &log_handler};
 
     ulogger_init(&logger, log_handlers, handler_data, (size_t) 1);
 
     ulogger_log(&logger, START_RADIO);
-    TEST_ASSERT_EQUAL(counter, 0);
+    test_counters(0);
     ulogger_log(&logger, START_RADIO);
-    TEST_ASSERT_EQUAL(counter, 1);
+    test_counters(1);
 }
-
-//int main(void) {
-//    UNITY_BEGIN();
-//
-//    RUN_TEST(test_sanity);
-//
-//    return UNITY_END();
-//}
--- ulogger/ulogger_async.c
+++ ulogger/ulogger_async.c
@@ -5,6 +5,7 @@ uLoggerAsyncErrorCode ulogger_async_init(uLoggerAsync *ulogger_async, char *star
                                          void** handlers_data, size_t num_handlers) {
     ulogger_init(ulogger_async->ulogger, handlers, handlers_data, num_handlers);
     ubuffer_init(ulogger_async->ubuffer, start, buffer_capacity);
+    return uLogger_Async_Sucess;
 }
 
 uLoggerAsyncErrorCode async_logger_log_event(uLoggerAsync *ulogger_async, EventType event_type, timestamp time,
@@ -29,4 +30,6 @@ uLoggerAsyncErrorCode async_logger_flush(uLoggerAsync *ulogger_async) {
         if (err_code) return (uLoggerAsyncErrorCode) err_code;
         ulogger_log_existing_event(ulogger_async->ulogger, event);
     }
+
+    return uLogger_Async_Sucess;
 }
