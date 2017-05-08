0a0e7327a2aa59a6b4c67db2670340fab4541e5a
Dima Gilburd <gilburd@gmail.com>
2017-05-08 14:31:06 +0300

    Added keen module for cc3200

--- ulogger/handlers/network_log_handler.c
+++ ulogger/handlers/network_log_handler.c
@@ -2,28 +2,15 @@
 #include "ubuffer.h"
 #include <string.h>
 #include <stdbool.h>
-
-#include "nrf_log.h"
-#define NETWORK_LOGGER_VERSION 1
-
-typedef struct {
-    uint32_t version;
-    EventType event_type;
-    timestamp time;
-    size_t data_length;
-} uLoggerEventHeader;
+#include "network_log_handler.h"
 
 HandlerReturnType network_handler_log(network_log_config * config, LogLevel level, EventType event_type, timestamp time, void * log_data, size_t data_length) {
     uLoggerEventHeader * stored_event = NULL;
 
-    NRF_LOG_INFO("network_handler_log %d\n", data_length);
     if (ubuffer_allocate_next(&config->buffer, (void **)&stored_event, sizeof(uLoggerEventHeader) + data_length)) {
-        
-        NRF_LOG_INFO("Failed to allocate\n");
-        return HANDLER_FAIL;
+              return HANDLER_FAIL;
     }
 
-    NRF_LOG_INFO("After alloc head is %d, empty is %d\n", config->buffer.head, config->buffer.num_empty_bytes_at_end);
     stored_event->version = NETWORK_LOGGER_VERSION;
     stored_event->time = time;
     stored_event->event_type = event_type;
@@ -36,34 +23,26 @@ HandlerReturnType network_handler_log(network_log_config * config, LogLevel leve
     return HANDLER_SUCCESS;
 }
 
-static void periodic_callback(void * data) {
+void netowork_logger_periodic_callback(void * data) {
     int err_code;
     
     uLoggerEventHeader * event;
     network_log_config * config = (network_log_config *) data;
-    NRF_LOG_INFO("Periodic callbaack %d %d\n", config->can_send(config->context), ubuffer_peek_first(&config->buffer, (void**)&event, sizeof(uLoggerEventHeader)));
-    NRF_LOG_INFO("Buffer size %d\n", config->buffer.size);
     while (config->can_send(config->context) && 
             ubuffer_peek_first(&config->buffer, (void**)&event, sizeof(uLoggerEventHeader)) == UBUFFER_SUCCESS) {
         uint16_t len = sizeof(uLoggerEventHeader) + event->data_length;
+        
         err_code = config->send(config->context, (void *) event, len);
-    
-        NRF_LOG_INFO("Got result %d\n", err_code);
+        
         if (err_code) {
             return;
         }
         ubuffer_free_first(&config->buffer, (void**)&event, len);
-        NRF_LOG_INFO("After free head: %d, empty %d\n", config->buffer.head, config->buffer.num_empty_bytes_at_end);
     }
 }
 
 int network_logger_init(network_log_config * config, uint8_t * buffer, size_t buffer_size) {
     int res;
     ubuffer_init(&config->buffer, (char *)buffer, buffer_size);
-    res = config->timer_init(config->log_send_period, periodic_callback);
-    if (res) {
-        return res;
-    }
-
     return 0;
 }
--- ulogger/handlers/network_log_handler.h
+++ ulogger/handlers/network_log_handler.h
@@ -1,49 +1,36 @@
-/**
- * @file
- * @brief Helper module for creating buffered network loggers.
- * The idea behind the network handler is that in an embedded environment all network logs will be buffered and sent in bursts
- * to reduce power usage and improve performance.
- */
 #ifndef NETWORK_LOG_HANDLER
 #define NETWORK_LOG_HANDLER
 
 #include "ubuffer.h"
 #include <stdbool.h>
 
-typedef void (*periodic_callback_function)(void * config);
 
 typedef int (*send_to_network)(void * network_context, uint8_t * data, uint32_t length);
 typedef bool (*can_send_to_network)(void * network_context);
-typedef int (*periodic_callback_init)(uint32_t time_in_ms, periodic_callback_function func);
+typedef void (*periodic_callback_function)(void * config);
 
 typedef struct {
     uint32_t log_send_period; 
     void * context;
     send_to_network send;
     can_send_to_network can_send;
-    periodic_callback_init timer_init;
+    periodic_callback_function callback;
     uBuffer buffer;
 } network_log_config;
 
-/**
- * @brief Initializes a network_log_config struct with the given buffer.
- * @param config The config struct, to be passed to the logger as handler context data
- * @param buffer
- * @param buffer_size
- * @return 0 on success, error code otherwise
- */
+#define NETWORK_LOGGER_VERSION 1
+
+typedef struct {
+    uint32_t version;
+    EventType event_type;
+    timestamp time;
+    size_t data_length;
+} uLoggerEventHeader;
+
 int network_logger_init(network_log_config * config, uint8_t * buffer, size_t buffer_size);
 
-/**
- * Log handler method for the ulogger log handler list.
- * @param config
- * @param level
- * @param event_type
- * @param time
- * @param log_data
- * @param data_length
- * @return
- */
+void netowork_logger_periodic_callback(void * data);
+
 HandlerReturnType network_handler_log(network_log_config * config, LogLevel level, EventType event_type, timestamp time, void * log_data, size_t data_length);
 
 #endif
