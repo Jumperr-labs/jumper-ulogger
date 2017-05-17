3f48484a84d3006c25eeea89e1efa33196e80929
Dimitri Gilburd <gilburd@gmail.com>
2017-05-17 17:29:58 +0300

    Added documentation for json formatter

--- platforms/cc3200/json_formatter.h
+++ platforms/cc3200/json_formatter.h
@@ -1,3 +1,9 @@
+/**@file
+ * @brief Json formatter.
+ * A json formatter used to format events before sending them to the events api.
+ * @ingroup cc3200_platform
+ * @{
+ */
 #ifndef JSON_FORMATTER_H
 #define JSON_FORMATTER_H
 
@@ -11,8 +17,14 @@ typedef struct {
     size_t buffer_length;
 } json_formatter_context;
 
-
+/**
+ * @brief A formatter implementation as defined in network_log_handler.h, to format your custom events
+ * you must implement a case in the formatter for each event.
+ */
 int json_formatter_format(void * formatter_context, uLoggerEventHeader * event,
                           uint8_t ** formatted_event, size_t * formatted_event_length);
 
 #endif //JSON_FORMATTER_H
+/**
+ * @}
+ */
